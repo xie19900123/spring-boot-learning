a69b0974030552e1259cc2116819f7fb78d64cb9
xiedeshou <499452441@qq.com>
2018-07-23 21:37:52 +0800

    (*)删除无效引用包

--- chapter-9/src/main/java/cn/lqdev/learning/springboot/chapter9/controller/UserController.java
+++ chapter-9/src/main/java/cn/lqdev/learning/springboot/chapter9/controller/UserController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
